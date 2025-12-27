a987af087257e5a337d887135490195e91fe658a
kwonwnsduf <junyeol1009@gmail.com>
2025-12-27 15:51:47 +0900

    Create 10818.cpp

---
+++ baekjoon/10818.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	int arr[1000000];
+	for (int i = 0;i < N;i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	cout << arr[0] << " " << arr[N - 1] << endl;
+}
