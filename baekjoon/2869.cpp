338f08db67bccf44420f6015dd31c36fcf6d0617
kwonwnsduf <junyeol1009@gmail.com>
2025-12-29 11:48:57 +0900

    Create 2869.cpp

---
+++ baekjoon/2869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	int a, b, v;
+	cin >> a >> b >> v;
+	int day = (v - b - 1) / (a - b) + 1;
+	cout << day << endl;
+
+}
