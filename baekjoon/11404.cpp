5e526f03237161d6cc2e3cd45f01323324b72cfc
kwonwnsduf <junyeol1009@gmail.com>
2026-03-05 14:17:02 +0900

    Create 11404.cpp

---
+++ baekjoon/11404.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1e9;
+int dist[101][101];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    // 1. 행렬 초기화
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) dist[i][j] = 0;
+            else dist[i][j] = INF;
+        }
+    }
+
+    // 2. 간선 정보 입력
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (c < dist[a][b]) dist[a][b] = c;
+    }
+
+    // 3. 플로이드-워셜 알고리즘 (k -> i -> j 순서 필수!)
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+
+    // 4. 결과 출력
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (dist[i][j] == INF) cout << 0 << " ";
+            else cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
